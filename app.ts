83ef2cf1fc33289194d77b64351f2f2819fad06f
tomoki-suruga <tomoki.s.0927@gmail.com>
2022-03-12 15:50:32 +0900

    fix package.json

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 const http = require("http").Server(app)
 const io = require("socket.io")(http)
 
