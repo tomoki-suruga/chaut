db7713666eda9fa6e66cc169892f34bd5dbadf15
YukiFujita <huji0327yuu@gmail.com>
2022-09-18 11:48:52 +0900

    add ioredis

--- app.ts
+++ app.ts
@@ -3,6 +3,10 @@ const app = express()
 const port = process.env.PORT || 5000
 const http = require("http").Server(app)
 const io = require("socket.io")(http)
+const moment = require('moment');
+const Redis = require('ioredis');
+const redis_address = process.env.REDIS_ADDRESS || 'redis://chaut-cluster.galiun.ng.0001.apne1.cache.amazonaws.com';
+const redis = new Redis(redis_address);
 
 app.get("/", (req: any, res: any) => {
   res.sendFile(__dirname + "/index.html")
@@ -13,6 +17,8 @@ io.on("connection", (socket: any) => {
     if (msg.userName === "") {
       msg.userName = "No Name"
     }
+    const result = redis.get('hoge')
+    console.log('redis_result: ',result);
     io.emit("chat message", msg)
   })
 })
