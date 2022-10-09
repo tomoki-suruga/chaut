f64ad26a35f9016be76fa85fb759736d2ae55690
YukiFujita <huji0327yuu@gmail.com>
2022-10-09 12:45:32 +0900

    add redis

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # chaut
 LIVE用チャットツール
+
+# 本番docker起動
+docker-compose up -d
+
+# ローカルdocker起動
+docker-compose -f docker-compose-redis.yml up -d
+
+# dockerコンテナにログイン
+docker ps
+docker exec -it dockerimageid sh
--- app.ts
+++ app.ts
@@ -1,26 +1,28 @@
+import type {Redis} from 'ioredis';
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
 const http = require("http").Server(app)
 const io = require("socket.io")(http)
-const moment = require('moment');
-const Redis = require('ioredis');
+const IoRedis = require('ioredis');
 const redis_address = process.env.REDIS_ADDRESS || 'redis://chaut-cluster.galiun.ng.0001.apne1.cache.amazonaws.com';
-const redis = new Redis(redis_address);
+const redis:Redis = new IoRedis(redis_address);
+/** for local */
+// const redis:Redis = new IoRedis('chaut_cache_1:6379');
 
 app.get("/", (req: any, res: any) => {
   res.sendFile(__dirname + "/index.html")
 })
 
 io.on("connection", (socket: any) => {
-  socket.on("chat message", (msg: { userName: string; comment: string }) => {
+  socket.on("chat message", async (msg: { userName: string; comment: string }) => {
     if (msg.userName === "") {
       msg.userName = "No Name"
     }
-    const result = redis.get('hoge')
-    console.log('redis_result: ',result);
+    redis.xadd('chaut', '*','message', 'hogehogehooge')
+    const respons = await redis.xread('COUNT', 100, 'STREAMS', 'chaut', 0)
     io.emit("chat message", msg)
   })
 })
 
-http.listen(port, () => console.log(`Example app listening on port ${port}!`))
+http.listen(port, () => console.log(`Example app listening on port ${port}!!!`))
