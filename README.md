ed2b95165accb83c29779e248924fc2db2f065b9
YukiFujita <huji0327yuu@gmail.com>
2022-12-10 12:16:32 +0900

    add reading redis and refactor

--- README.md
+++ README.md
@@ -4,9 +4,21 @@ LIVE用チャットツール
 # 本番docker起動
 docker-compose up -d
 
-# ローカルdocker起動
+# ローカルredis用docker起動
 docker-compose -f docker-compose-redis.yml up -d
+
+# ローカルwebappのdocker起動(app.tsのライブラリ読み込みのコメントアウトの修正する)
+docker-compose -f docker-compose.local.yml up
 
 # dockerコンテナにログイン
 docker ps
 docker exec -it dockerimageid sh
+
+# redis cli起動
+redis-cli
+
+# xread
+実行例)
+# chaut : キー名
+# 0-0 : ID指定(指定ID以降の全てのデータを取得)
+xread streams chaut 0-0
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import type {Redis} from 'ioredis';
+import type {Redis, Result} from 'ioredis';
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
@@ -6,9 +6,9 @@ const http = require("http").Server(app)
 const io = require("socket.io")(http)
 const IoRedis = require('ioredis');
 const redis_address = process.env.REDIS_ADDRESS || 'redis://chaut-cluster.galiun.ng.0001.apne1.cache.amazonaws.com';
-const redis:Redis = new IoRedis(redis_address);
+// const redis:Redis = new IoRedis(redis_address);
 /** for local */
-// const redis:Redis = new IoRedis('chaut_cache_1:6379');
+const redis:Redis = new IoRedis('chaut_cache_1:6379');
 
 app.get("/", (req: any, res: any) => {
   res.sendFile(__dirname + "/index.html")
@@ -19,9 +19,18 @@ io.on("connection", (socket: any) => {
     if (msg.userName === "") {
       msg.userName = "No Name"
     }
-    redis.xadd('chaut', '*','message', 'hogehogehooge')
+    redis.xadd('chaut', '*', msg.userName, msg.comment)
     const respons = await redis.xread('COUNT', 100, 'STREAMS', 'chaut', 0)
     io.emit("chat message", msg)
+  })
+
+  socket.on("init",async () => {
+    const response = await redis.xread('STREAMS', 'chaut', 0)
+    const [key, message] = response !== null ? response[0]: [undefined, []];
+    const msgs = message.map(m => ({'userName': m[1][0], 'comment': m[1][1]}))
+    console.log('msgs', msgs)
+    console.log('key', key)
+    io.emit("init", msgs)
   })
 })
 
